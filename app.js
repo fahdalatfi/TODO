cb33004095b7a690d3109ca4d0536099d1556554
fahdalatfi <alatfifahd2@gmail.com>
2021-11-19 02:37:00 +0200

    adding removing function

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 function Adding(){
     let li = document.createElement('li')
-    li.innerText =  document.getElementById('text').value;
-    document.getElementById('list').appendChild(li);
-    
+    let value = document.getElementById('text').value;
+
+    if(value === ""){
+        alert("Don't leave the box empty!");
+    } else {
+        li.innerText = value;
+        document.getElementById('list').appendChild(li);
+        li.setAttribute("id", "lil");
+    }
+
 }
+
 
 let btn = document.getElementById('btn');
 btn.addEventListener('click', Adding)
+
+
+
+function removing(){
+    let li = document.getElementById('lil');
+    li.remove();
+}
+
+let btn1 = document.getElementById('btn1')
+btn1.addEventListener('click', removing)
+
+
 
